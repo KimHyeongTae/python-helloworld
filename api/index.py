641cde82decea551bcf9cf4799f33102b41ef5c6
Marcus <xo41555558@gmail.com>
2023-04-20 12:19:55 +0900

    googleapiclent -> google-api-python-client로 수정

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import os
 from http.server import BaseHTTPRequestHandler
-from googleapiclient.discovery import build
+from google-api-python-client.discovery import build
 import psycopg2
 
 DEVELOPER_KEY = os.environ.get('DEVELOPER_KEY')
