ceb78909216d6c81574b885b42ab8bc326da6ccb
Marcus <xo41555558@gmail.com>
2023-04-20 15:36:54 +0900

    main.py 수정

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
+import json
 import os
-from http.server import BaseHTTPRequestHandler
-# from googleapiclient.discovery import build
 import psycopg2
 
 api_key = os.environ.get('DEVELOPER_KEY')
@@ -11,78 +10,72 @@ user = os.environ.get('user')
 password = os.environ.get('password')
 
 
-class handler(BaseHTTPRequestHandler):
+def update_channel_stats():
+    # 채널 ID 리스트 불러오기
+    with open('data/channels_renewal_202304031340.csv', 'r') as f:
+        channel_id_list = f.read().splitlines()
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+    # youtube API client 생성
+    DEVELOPER_KEY = api_key
+    YOUTUBE_API_SERVICE_NAME = 'youtube'
+    YOUTUBE_API_VERSION = 'v3'
 
-        # youtube data api 사용을 위한 인증키 로드
-        DEVELOPER_KEY = api_key
-        YOUTUBE_API_SERVICE_NAME = 'youtube'
-        YOUTUBE_API_VERSION = 'v3'
+    # 빈 리스트 생성
+    channel_data = []
 
-        def update_channel_stats():
-            # 채널 ID 리스트 불러오기
-            with open('data/channels_renewal_202304031340.csv', 'r') as f:
-                channel_id_list = f.read().splitlines()
+    # 채널 ID 리스트를 순회하며 API 호출하여 데이터 추출
+    for channel_id in channel_id_list:
+        try:
+            search_response = youtube.channels().list(
+                # 'channel_id'를 대상으로 'snippet', 'statistics' 검색
+                part='id,snippet,statistics',
+                id=channel_id,
+                maxResults=50
+            ).execute()
 
-            # youtube API client 생성
-            youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+            # search_response에서 statistics 및 snippet 데이터 추출
+            stats = search_response['items'][0]['statistics']
+            snippet = search_response['items'][0]['snippet']
 
-            # 빈 리스트 생성
-            channel_data = []
+            # stats 및 snippet dictionary에서 데이터 정리
+            channel = {
+                'channel_id': channel_id,
+                'view_count': stats['viewCount'],
+                'subscriber_count': stats['subscriberCount'],
+                'video_count': stats['videoCount'],
+                'title': snippet['title']
+            }
 
-            # 채널 ID 리스트를 순회하며 API 호출하여 데이터 추출
-            for channel_id in channel_id_list:
-                try:
-                    search_response = youtube.channels().list(
-                        # 'channel_id'를 대상으로 'snippet', 'statistics' 검색
-                        part='id,snippet,statistics',
-                        id=channel_id,
-                        maxResults=50
-                    ).execute()
+            channel_data.append(channel)
 
-                    # search_response에서 statistics 및 snippet 데이터 추출
-                    stats = search_response['items'][0]['statistics']
-                    snippet = search_response['items'][0]['snippet']
+        except:
+            continue
 
-                    # stats 및 snippet dictionary에서 데이터 정리
-                    channel = {
-                        'channel_id': channel_id,
-                        'view_count': stats['viewCount'],
-                        'subscriber_count': stats['subscriberCount'],
-                        'video_count': stats['videoCount'],
-                        'title': snippet['title']
-                    }
+    # postgresql에 연결
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password
+    )
 
-                    channel_data.append(channel)
+    # 데이터프레임을 postgresql에 삽입
+    cur = conn.cursor()
+    for channel in channel_data:
+        cur.execute(
+            "INSERT INTO channel_stats (channel_id, channel_title, subscriber_count, view_count, video_count) VALUES (%s, %s, %s, %s, %s)",
+            (channel['channel_id'], channel['title'], channel['subscriber_count'], channel['view_count'],
+             channel['video_count']))
+    conn.commit()
+    cur.close()
+    conn.close()
 
-                except:
-                    continue
 
-            # postgresql에 연결
-            conn = psycopg2.connect(
-                host=host,
-                port=port,
-                database=database,
-                user=user,
-                password=password
-            )
+def handler(event, context):
+    update_channel_stats()
 
-            # 데이터프레임을 postgresql에 삽입
-            cur = conn.cursor()
-            for channel in channel_data:
-                cur.execute(
-                    "INSERT INTO channel_stats (channel_id, channel_title, subscriber_count, view_count, video_count) VALUES (%s, %s, %s, %s, %s)",
-                    (channel['channel_id'], channel['title'], channel['subscriber_count'], channel['view_count'],
-                     channel['video_count']))
-            conn.commit()
-            cur.close()
-            conn.close()
-
-        update_channel_stats()
-
-        return
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Vercel!')
+    }
